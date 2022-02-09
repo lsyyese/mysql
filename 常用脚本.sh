00fd8992b36b3338c170153aed8a7f16098f2bdd
lsyyese <994405272@qq.com>
2022-02-09 00:49:49 -0600

    Update 常用脚本.sh
    1

--- 常用脚本.sh
+++ 常用脚本.sh
@@ -7,3 +7,45 @@ mysql -h$_IP -udevops_bbk -p'' -P$_PORT mysql -e "show slave status\G; show vari
 
 
 done <list.txt
+#########################
+
+function autossh()
+{       filepath="$1"
+        expect -c "
+                set timeout -1
+                spawn  scp -P 44422  $filepath dbatmp@10.35.185.8:/tmp
+                expect {
+                          \"(yes/no)?\"                         {send \"yes\r\";exp_continue}
+                          \"password:\"                        {send \"$remotePwd\r\";exp_continue}
+						  \"wish to overwrite\"                 {send \"y\r\";exp_continue}
+                          \"FATAL\"                             {exit 2;exp_continue}
+                          timeout                               {exit 2;exp_continue}
+                          \"No route to host\"                  {exit 2;exp_continue}
+                          \"Connection Refused\"                {exit 2;exp_continue}
+                          \"Connection refused\"                {exit 2;exp_continue}
+                          \"Host key verification failed\"      {exit 2;exp_continue}
+                          \"Illegal host key\"                  {exit 2;exp_continue}
+                          \"Connection Timed Out\"              {exit 2;exp_continue}
+                          \"Connection timed out\"              {exit 2;exp_continue}
+                          \"Interrupted system call\"           {exit 2;exp_continue}
+                          \"Disconnected; connection lost\"     {exit 2;exp_continue}
+                          \"Authentication failed\"             {exit 2;exp_continue}
+                          \"File exists\"                       {exit 2;exp_continue}
+                          \"Error\"                             {exit 2;exp_continue}
+                          \"ERROR\"                             {exit 2;exp_continue}
+                          \"error\"                             {exit 2;exp_continue}
+                          \"No such file\"                      {exit 2;exp_continue}
+                          \"Permission denied\"                 {exit 2;exp_continue}
+                          \"Destination Unreachable\"           {exit 2}
+                       }
+        "
+		if [[ $? -ne 0 ]];then
+           echo "scp执行错误退出"
+           exit 1
+
+        fi
+}
+
+remotePwd='MKJJSabc2343434'
+filepath=$1  
+autossh  $filepath
